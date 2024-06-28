c0ff7a92899a108603ee5a99592187cc4b286150
ernurQ <garifullin.ernur@gmail.com>
2024-06-28 21:45:26 +0500

    feat(tasks): add create task request validation

--- backend/src/tasks/dto/create-task.dto.ts
+++ backend/src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator'
+
 export class CreateTaskDto {
+  @IsString()
   title: string
+
+  @IsString()
   content: string
+
+  @IsString()
   projectId: string
 }
