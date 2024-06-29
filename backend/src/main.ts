dd4e0888537416a76bf86122286289e10d91d827
ernurQ <garifullin.ernur@gmail.com>
2024-06-29 19:47:22 +0500

    feat: add swagger config to app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  const config = new DocumentBuilder()
+    .setTitle('Earth note backend API')
+    .setVersion('1.0')
+    .addTag('Earth note backend API')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   app.useGlobalPipes(
     new ValidationPipe({
