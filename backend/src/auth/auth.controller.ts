06343a76e7c74a3624839ab79e59b6e7ee9aaf2d
ernurQ <garifullin.ernur@gmail.com>
2024-06-29 22:11:03 +0500

    feat(auth): add swagger

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,18 +1,56 @@
-import { AuthService } from '@Auth/auth.service'
-import { loginRequestDto, RegisterDto } from '@Auth/dto'
-import { LocalAuthGuard } from '@Auth/local-auth.guard'
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common'
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger'
 
+import { AuthService } from '@Auth/auth.service'
+import {
+  LoginDto,
+  loginRequestDto,
+  RegisterDto,
+  RegisterResponseDto,
+} from '@Auth/dto'
+import { LocalAuthGuard } from '@Auth/local-auth.guard'
+
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiBody({ type: LoginDto })
+  @ApiOkResponse({ description: 'Login successful' })
+  @ApiBadRequestResponse({
+    description: 'Username and password must be string',
+  })
+  @ApiUnauthorizedResponse({ description: 'Incorrect password' })
+  @ApiNotFoundResponse({ description: 'User not found' })
   @UseGuards(LocalAuthGuard)
   @Post('/login')
+  @HttpCode(200)
   async login(@Req() req: loginRequestDto) {
     return this.authService.login(req.user)
   }
 
+  @ApiBody({ type: RegisterDto })
+  @ApiCreatedResponse({
+    description: 'Register successful',
+    type: RegisterResponseDto,
+  })
+  @ApiConflictResponse({ description: 'Username already exists' })
   @Post('/register')
   async register(@Body() registerUserDto: RegisterDto) {
     return this.authService.register(registerUserDto)
